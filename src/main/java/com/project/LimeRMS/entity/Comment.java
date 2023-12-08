0e1f6a931a4ee631964824ba93a6070628acfaad
snowfluppy <snowfluppy@ajou.ac.kr>
2023-12-08 23:17:24 +0900

    etc: Comment 엔티티 추가

---
+++ src/main/java/com/project/LimeRMS/entity/Comment.java
@@ -0,0 +1,60 @@
+package com.project.LimeRMS.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment {
+    @Id
+    @Column(nullable = false)
+    private Long commendId;
+
+    //수정 필요
+    @Column(nullable = false)
+    private String userId;
+
+    //수정 필요
+    @Column(nullable = false)
+    private Long contentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "commendId", nullable = false)
+    @JsonIgnore
+    private Comment hiComment;
+
+    @Column
+    private String comment;
+
+    @Column(columnDefinition = "TINYINT default 0")
+    private Long score;
+
+    @CreatedDate
+    @Column
+    private LocalDateTime regDt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modfDt;
+
+    @Column
+    private String modfUserId;
+
+    @Column
+    private String image;
+
+    @Column(columnDefinition = "VARCHAR(1) default 'N'")
+    private String delYn;
+}
