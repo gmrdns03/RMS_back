c3ee6b0b3a4a4e0cb6d002daee3500edb9ef4c6e
snowfluppy <snowfluppy@ajou.ac.kr>
2023-12-10 23:56:40 +0900

    etc: Notification 엔티티 추가

---
+++ src/main/java/com/project/LimeRMS/entity/Notification.java
@@ -0,0 +1,60 @@
+package com.project.LimeRMS.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "Notification")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer notiId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiverId", referencedColumnName = "userId", nullable = false)
+    private User receiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "senderId", referencedColumnName = "userId", nullable = false)
+    private User sender;
+
+    @Column
+    private String notiType;
+
+    @Column
+    private String notiContent;
+
+    @Column
+    private String notiUrl;
+
+    @Column
+    private String notiStatus;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime joinDt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime modfDt;
+
+    @Column
+    private String modfUserId;
+}
