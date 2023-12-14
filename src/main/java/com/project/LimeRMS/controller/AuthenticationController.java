d4d2b965bdbb8088f32eeeae2651290b07965850
snowfluppy <snowfluppy@ajou.ac.kr>
2023-12-14 11:02:50 +0900

    fix: tag 추가를 통한 swagger api 호출 문제 해결  - SwaggerConfig 파일 삭제

--- src/main/java/com/project/LimeRMS/controller/AuthenticationController.java
+++ src/main/java/com/project/LimeRMS/controller/AuthenticationController.java
@@ -7,25 +7,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "AuthenticationController", description = "AuthenticationController API 모음.")
-@Controller
+@Tag(name = "AuthenticationController", description = "AuthenticationController API")
+@RestController
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private AuthenticationService authenticationService;
 
     //Test 코드(나중에 연관 코드 삭제 필요)
-//    @PostMapping("/test")
-//    @ResponseBody
-//    public boolean insertAuthentication(@RequestBody Authentication authentication) {
-//        try {
-//            authenticationService.insertAuthentication(authentication);
-//        } catch (Exception e) {
-//            System.out.println("Exception occurred: " + e.getMessage());
-//            return false;
-//        }
-//        return true;
-//    }
+    @PostMapping("/test")
+    @ResponseBody
+    public boolean insertAuthentication(@RequestBody Authentication authentication) {
+        try {
+            authenticationService.insertAuthentication(authentication);
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
 
     @GetMapping("/testTest")
     public String example2() {
--- src/main/java/com/project/LimeRMS/controller/UserController.java
+++ src/main/java/com/project/LimeRMS/controller/UserController.java
@@ -5,13 +5,10 @@ import com.project.LimeRMS.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "UserController", description = "UserController API 모음.")
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserController {
     private UserService userService;
