18f873bbebec8cccababe549a294e20d03140fd4
Minkyo_Kim <mkkim@euclidsoft.co.kr>
2024-01-11 13:14:54 +0900

    etc:cors 수정4

--- src/main/java/com/project/LimeRMS/security/WebConfig.java
+++ src/main/java/com/project/LimeRMS/security/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://110.35.15.168:8089")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
