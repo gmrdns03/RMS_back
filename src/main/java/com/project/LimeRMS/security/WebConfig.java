adbf860fb0c90087c0084623b6db8d6f9cdd1148
Minkyo_Kim <mkkim@euclidsoft.co.kr>
2024-01-11 14:21:26 +0900

    etc:cors 수정6

--- src/main/java/com/project/LimeRMS/security/WebConfig.java
+++ src/main/java/com/project/LimeRMS/security/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(MAX_AGE_SECS);
